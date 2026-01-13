246a956451f7cda4ab1f9bae899557a4a1fa8b88
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-01-13 19:28:37 +0530

    [INJIMOB-3598] fix: syntax issue
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/VCIClient/authorizationCodeFlow/InteractiveAuthorization/Handler/InteractiveAuthorizationHandler.swift
+++ Sources/VCIClient/authorizationCodeFlow/InteractiveAuthorization/Handler/InteractiveAuthorizationHandler.swift
@@ -12,7 +12,7 @@ final class InteractiveAuthorizationHandler {
         clientMetadata: ClientMetadata,
         credentialConfigurationId: String,
         authorizationMethods: [AuthorizationMethod],
-        pkceSession: PKCESessionManager.PKCESession,
+        pkceSession: PKCESessionManager.PKCESession
     ) async throws -> AuthorizationResponse {
         
         do {
