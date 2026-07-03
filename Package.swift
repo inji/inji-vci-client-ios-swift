04337176acab8fe9e7b9590171882b7e4efd8009
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-07-03 12:32:40 +0530

    refactor: update ovp package version
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/valpackett/SwiftCBOR", .upToNextMajor(from: "0.5.0")),
-        .package(url: "https://github.com/inji/inji-openid4vp-ios-swift", branch: "develop")
+        .package(url: "https://github.com/inji/inji-openid4vp-ios-swift", branch: "release-1.0.x")
     ],
     targets: [
         .target(
